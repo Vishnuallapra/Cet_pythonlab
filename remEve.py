4fa6999fe8dfcdc792cd60983ade2064b80325d6
Vishnuallapra <93085896+Vishnuallapra@users.noreply.github.com>
2021-12-15 11:51:20 +0530

    Delete remEve.py

--- remEve.py
+++
@@ -1,8 +0,0 @@
-myList=list(map(int,input("Enetr random numbers: ").split()))
-print("List: ",myList)
-newList=[]
-for i in myList:
-    if i%2!=0:
-        newList.append(i)
-
-print("List After Removing Even Numbers: ",newList)
