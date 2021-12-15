e0dda39d25c4f7957a947852a31d6e3cc802a43b
Vishnuallapra <93085896+Vishnuallapra@users.noreply.github.com>
2021-12-15 12:09:36 +0530

    Delete count.py

--- count.py
+++
@@ -1,7 +0,0 @@
-str=input("Enter String: ").split()
-for i in set(str):
-    count=1
-    for k in range(str.index(i) + 1,len(str)):
-        if i==str[k] :
-            count+=1
-    print(i," occurs ",count," Times")
